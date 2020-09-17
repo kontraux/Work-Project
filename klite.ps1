15d76e5a840288fa8ede31c6ebdbd6780f0ccf15
kontraux <69282807+kontraux@users.noreply.github.com>
2020-09-16 19:17:44 -0500

    fixed choco scripts, wifi button

--- klite.ps1
+++ klite.ps1
@@ -1,4 +1,5 @@
-﻿    Write-Host "Attempting installation..."
+﻿    if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+    Write-Host "Attempting installation..."
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex | Out-Host
     choco upgrade k-litecodecpackbasic -y | Out-Host
     Write-Host "Operation complete!"
--- silverlight.ps1
+++ silverlight.ps1
@@ -1,4 +1,5 @@
-﻿    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex | Out-Host
+﻿    if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex | Out-Host
     Write-Host "Attempting Installation..."
     choco upgrade Silverlight -y | Out-Host
     Write-Host "Operation complete!"
